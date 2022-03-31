e5b7d4bc92aecf728db4180bf1946e0021995c6a
Rodrigo <rodrigokaconde@yahoo.com.br>
2022-03-30 21:35:33 -0300

    create project

---
+++ src/app/add/add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ivory-add',
+  templateUrl: './add.component.html',
+  styleUrls: ['./add.component.css']
+})
+export class AddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './add/add.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AddComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
